adcb7b946998ba2738726270f0026b6eca925135
shlimsady <federicomoret23@gmail.com>
2024-04-29 15:37:47 -0300

    newest commit

--- .learn/exercises/07.1-test-post-todo/solution.hide.py
+++ .learn/exercises/07.1-test-post-todo/solution.hide.py
@@ -3,11 +3,9 @@ app = Flask(__name__)
 
 todos = [{"label": "A dummy todo", "done": True}]
 
-
 @app.route('/todos', methods=['GET'])
 def hello_world():
     return jsonify(todos)
-
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
@@ -15,7 +13,5 @@ def add_new_todo():
     print("Incoming request with the following body", request_body)
     return 'Response for the POST todo'
 
-
-# These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
